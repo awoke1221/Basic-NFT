b2a56a39e7c49526b9984297d0a6162e56dcf808
Awoke Zemenu Getahun <awokezemenu8@gmail.com>
2023-10-20 13:41:09 +0300

    writing a smart contract to crate AZ nft tokens

--- contracts/basicNFT.sol
+++ contracts/basicNFT.sol
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >= 0.7.0 < 0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract AZNFT is ERC721 {
+    uint256 tokenIdCounter;
+    constructor() ERC721("AZtoken", "AZt"){
+              
+    }
+
+    function mintNFT() public returns (uint256) {
+         tokenIdCounter++;
+         uint256 newTokenId = tokenIdCounter;
+        _mint(msg.sender, newTokenId);
+         return newTokenId;
+
+        
+    }
+}
